e6f92268aaa7b644c1463bb5e45466558978a23c
KrumovOOD <krumov_ood@abv.bg>
2018-10-29 16:10:26 -0700

    More Nested Conditional Statements - Lab

---
+++ BasicExercises/FruitShop/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FruitShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fruit = Console.ReadLine();
+            string dayOfWeek = Console.ReadLine();
+            double quantity = double.Parse(Console.ReadLine());
+            double price = -1.0;
+
+            if (dayOfWeek == "Monday" || dayOfWeek == "Tuesday" || dayOfWeek == "Wednesday" || dayOfWeek == "Thursday" || dayOfWeek == "Friday")
+            {
+                if (fruit == "banana")
+                {
+                    Console.WriteLine($"{quantity * 2.50:F2}");
+                }
+                else
+
+                if (fruit == "apple")
+                {
+                    Console.WriteLine($"{quantity * 1.20:F2}");
+                }
+                else
+
+                if (fruit == "orange")
+                {
+                    Console.WriteLine($"{quantity * 0.85:F2}");
+                }
+                else
+
+                if (fruit == "grapefruit")
+                {
+                    Console.WriteLine($"{quantity * 1.45:F2}");
+                }
+                else
+
+                if (fruit == "kiwi")
+                {
+                    Console.WriteLine($"{quantity * 2.70:F2}");
+                }
+                else
+
+                if (fruit == "pineapple")
+                {
+                    Console.WriteLine($"{quantity * 5.50:F2}");
+                }
+                else
+
+                if (fruit == "grapes")
+                {
+                    Console.WriteLine($"{quantity * 3.85:F2}");
+                }
+                else
+                    Console.WriteLine("error");
+
+            }
+
+            else
+
+            if (dayOfWeek == "Saturday" || dayOfWeek == "Sunday")
+            {
+                if (fruit == "banana")
+                {
+                    Console.WriteLine($"{quantity * 2.70:F2}");
+                }
+                else
+
+                if (fruit == "apple")
+                {
+                    Console.WriteLine($"{quantity * 1.25:F2}");
+                }
+                else
+
+                if (fruit == "orange")
+                {
+                    Console.WriteLine($"{quantity * 0.90:F2}");
+                }
+                else
+
+                if (fruit == "grapefruit")
+                {
+                    Console.WriteLine($"{quantity * 1.60:F2}");
+                }
+                else
+
+                if (fruit == "kiwi")
+                {
+                    Console.WriteLine($"{quantity * 3.00:F2}");
+                }
+                else
+
+                if (fruit == "pineapple")
+                {
+                    Console.WriteLine($"{quantity * 5.60:F2}");
+                }
+                else
+
+                if (fruit == "grapes")
+                {
+                    Console.WriteLine($"{quantity * 4.20:F2}");
+                }
+            }
+            else Console.WriteLine("error");
+
+
+        }
+    }
+}
---
+++ BasicExercises/TradeCommisions/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradeCommisions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string city = Console.ReadLine().ToLower();
+            double sales = double.Parse(Console.ReadLine());
+            double discount;
+
+            if (city == "sofia")
+            {
+                if (sales >= 0 && sales <= 500)
+                {
+                    Console.WriteLine($"{discount = (sales * 5) / 100:F2}");
+                }
+                else
+
+                if (sales > 500 && sales <= 1000)
+                {
+                    Console.WriteLine($"{discount = (sales * 7) / 100:F2}");
+                }
+                else
+
+                if (sales > 1000 && sales <= 10000)
+                {
+                    Console.WriteLine($"{discount = (sales * 8) / 100:F2}");
+                }
+                else
+
+                if (sales > 10000)
+                {
+                    Console.WriteLine($"{discount = (sales * 12) / 100:F2}");
+                }
+                else
+                    Console.WriteLine("error");
+
+            }
+            else
+
+            if (city == "varna")
+            {
+                if (sales >= 0 && sales <= 500)
+                {
+                    Console.WriteLine($"{discount = (sales * 4.5) / 100:F2}");
+                }
+                else
+
+                if (sales > 500 && sales <= 1000)
+                {
+                    Console.WriteLine($"{discount = (sales * 7.5) / 100:F2}");
+                }
+                else
+
+                if (sales > 1000 && sales <= 10000)
+                {
+                    Console.WriteLine($"{discount = (sales * 10) / 100:F2}");
+                }
+                else
+
+                if (sales > 10000)
+                {
+                    Console.WriteLine($"{discount = (sales * 13) / 100:F2}");
+                }
+                else
+                    Console.WriteLine("error");
+
+
+            }
+            else
+
+            if (city == "plovdiv")
+            {
+                if (sales >= 0 && sales <= 500)
+                {
+                    Console.WriteLine($"{discount = (sales * 5.5) / 100:F2}");
+                }
+                else
+
+                if (sales > 500 && sales <= 1000)
+                {
+                    Console.WriteLine($"{discount = (sales * 8) / 100:F2}");
+                }
+                else
+
+                if (sales > 1000 && sales <= 10000)
+                {
+                    Console.WriteLine($"{discount = (sales * 12) / 100:F2}");
+                }
+                else
+
+                if (sales > 10000)
+                {
+                    Console.WriteLine($"{discount = (sales * 14.5) / 100:F2}");
+                }
+                else
+                    Console.WriteLine("error");
+
+
+            }
+            else Console.WriteLine("error");
+        }
+
+    }
+}
